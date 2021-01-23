9f08d0c21fdc2be2817fd8b6aa3e3947f0f16deb
Rratic <panjiacheng@aliyun.com>
2021-01-23 16:37:28 +0800

    小游戏基础可玩性

--- script/queen_game_main.js
+++ script/queen_game_main.js
@@ -1,24 +1,57 @@
-var length = 8, height = 9, qsize = 64;
-var queen_1_x = 0, queen_1_y = 0, queen_1_show = "#FF4500";
-var queen_2_x = 7, queen_2_y = 8, queen_2_show = "#45FF00";
-var thiscanvas, draw;
+var length = 8, height = 9, qsize = 64, mox, moy, thisq = 1, tx, ty, ex, ey, tc, bc = "#57579F";
+var queen_1_x = 0, queen_1_y = 0, queen_1_show = "#EF4507";
+var queen_2_x = 7, queen_2_y = 8, queen_2_show = "#45EF07";
+var thiscanvas, draw, info;
+function loadmo(e) {
+    mox = Math.floor(e.clientX / qsize);
+    moy = Math.floor(e.clientY / qsize);
+}
 function cnvs_getCoordinates(e) {
-    x = Math.floor(e.clientX / qsize);
-    y = Math.floor(e.clientY / qsize);
-    document.getElementById("xycoordinates").innerHTML = "Coordinates: (" + x + "," + y + ")";
+    loadmo(e);
+    document.getElementById("xycoordinates").innerHTML = "坐标：(" + mox + "," + moy + ")";
 }
 function cnvs_clearCoordinates() {
     document.getElementById("xycoordinates").innerHTML = "";
+}
+function tellwiner() {
+    switch (thisq) {
+        case 1: info = "胜者：先手"; break;
+        case 2: info = "胜者：后手"; break;
+    }
+    document.getElementById("gameinfo").innerHTML = info;
 }
 function fillqueen(x, y, color) {
     draw.fillStyle = color;
     draw.fillRect(x * qsize, y * qsize, x * qsize + qsize - 1, y * qsize + qsize - 1);
 }
-function queengame() {
-    thiscanvas = document.getElementById("queen_game");
-    draw = thiscanvas.getContext("2d");
-    draw.fillStyle = "#5757AF";
+function picinit() {
+    draw.fillStyle = bc;
     draw.fillRect(0, 0, length * qsize - 1, height * qsize - 1);
     fillqueen(queen_1_x, queen_1_y, queen_1_show);
     fillqueen(queen_2_x, queen_2_y, queen_2_show);
+}
+function queeninit() {
+    thiscanvas = document.getElementById("queen_game");
+    draw = thiscanvas.getContext("2d");
+    picinit();
+}
+function queenmove(e) {
+    loadmo(e);
+    if (thisq == 1) { tx = queen_1_x; ty = queen_1_y; tc = queen_1_show; ex = queen_2_x; ey = queen_2_y; }
+    else { tx = queen_2_x; ty = queen_2_y; tc = queen_2_show; ex = queen_1_x; ey = queen_1_y; }
+    if (tx != mox && ty != moy && tx - mox != ty - moy && tx - mox != moy - ty) {
+        window.alert("无效的移动！");
+        return;
+    }
+    if (mox == ex && moy == ey) {
+        tellwiner();
+        window.alert(info);
+        queen_1_x = 0; queen_1_y = 0; queen_2_x = 7; queen_2_y = 8; thisq = 1;
+        picinit();
+    }
+    fillqueen(tx, ty, bc);
+    fillqueen(mox, moy, tc);
+    if (thisq == 1) { queen_1_x = mox; queen_1_y = moy; }
+    else { queen_2_x = mox; queen_2_y = moy; }
+    thisq = 3 - thisq;
 }
